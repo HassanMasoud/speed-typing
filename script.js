5772e215a0d90ad9e396aa41af3035967910fa83
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-03 22:14:07 +0300

    Change quote to single characters

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ function getRandomQuote() {
 
 async function renderNewQuote() {
   const quote = await getRandomQuote();
-  quoteDisplayElement.innerText = quote;
+  quoteDisplayElement.innerHTML = "";
+  quote.split("").forEach((character) => {
+    const characterSpan = document.createElement("span");
+    characterSpan.innerText = character;
+    quoteDisplayElement.appendChild(characterSpan);
+  });
   quoteInputElement.innerText = null;
 }
 
